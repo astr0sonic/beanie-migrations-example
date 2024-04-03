281ab884f3600e994fad6371d5cec096b1b6d162
astrosonic <z.astrosonic@gmail.com>
2024-04-04 01:37:25 +0300

    init

---
+++ mongo/init_rs.sh
@@ -0,0 +1,11 @@
+until mongosh --host ${MONGO_HOST}:${MONGO_PORT} -u ${MONGO_USERNAME} -p ${MONGO_PASSWORD} > /dev/null 2>&1
+do
+    echo "Waiting for primary..."
+    sleep 1
+done
+mongosh --host ${MONGO_HOST}:${MONGO_PORT} -u ${MONGO_USERNAME} -p ${MONGO_PASSWORD} --eval "rs.initiate({
+    _id: '${MONGO_REPLICA_SET}',
+    members: [
+        {_id: 0, host: '${MONGO_HOST}:${MONGO_PORT}'}
+    ]
+})"
